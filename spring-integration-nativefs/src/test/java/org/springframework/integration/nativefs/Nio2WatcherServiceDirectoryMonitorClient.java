12c7d9eeab974a532b01e4befabe68d3c0cb487a
Josh Long <josh.long@springsource.com>
2010-12-12 22:46:47 -0800

    commenting out the NIO2 code until i can confgure a x-platform build system thatll let me use JDK 7 on Linux (where its available) and on OSX use JDK6 and the native code

--- spring-integration-nativefs/src/test/java/org/springframework/integration/nativefs/Nio2WatcherServiceDirectoryMonitorClient.java
+++ spring-integration-nativefs/src/test/java/org/springframework/integration/nativefs/Nio2WatcherServiceDirectoryMonitorClient.java
@@ -1,7 +1,7 @@
 package org.springframework.integration.nativefs;
 
 import org.springframework.integration.nativefs.fsmon.DirectoryMonitor;
-import org.springframework.integration.nativefs.fsmon.Nio2WatchServiceDirectoryMonitor;
+//import org.springframework.integration.nativefs.fsmon.Nio2WatchServiceDirectoryMonitor;
 
 /**
  * Simple implementation of {@link AbstractDirectoryMonitorClient}
@@ -9,6 +9,7 @@ import org.springframework.integration.nativefs.fsmon.Nio2WatchServiceDirectoryM
  * @author Josh Long
  */
 public class Nio2WatcherServiceDirectoryMonitorClient extends AbstractDirectoryMonitorClient {
+	/*
 	@Override
 	protected DirectoryMonitor initDirectoryMonitor() throws Throwable {
 		Nio2WatchServiceDirectoryMonitor directoryMonitor = new Nio2WatchServiceDirectoryMonitor();
@@ -24,4 +25,10 @@ public class Nio2WatcherServiceDirectoryMonitorClient extends AbstractDirectoryM
 
 		watcherServiceDirectoryMonitorClient.start();
 	}
+	 */
+
+    @java.lang.Override
+    protected DirectoryMonitor initDirectoryMonitor() throws Throwable {
+        return null;
+    }
 }
