c9bc16742cac961ffe29976287b576368168a672
Josh Long <josh.long@springsource.com>
2011-03-02 05:00:14 -0800

    including code

---
+++ spring-integration-smpp/src/main/java/org/springframework/integration/smpp/SmppInboundGateway.java
@@ -0,0 +1,12 @@
+package org.springframework.integration.smpp;
+
+/**
+ *
+ * what's an inbound gateway in this case? Receive a text message and then automatically send a response.
+ *
+ * @author Josh Long
+ * @since 2.1
+ *
+ */
+public class SmppInboundGateway {
+}
