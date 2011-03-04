cd0baed3e660ebc028459e2d86ad205b88a8c45d
Josh Long <josh.long@springsource.com>
2011-03-04 15:14:58 -0800

    more test changes

--- spring-integration-smpp/src/main/java/org/springframework/integration/smpp/SmppInboundChannelAdapter.java
+++ spring-integration-smpp/src/main/java/org/springframework/integration/smpp/SmppInboundChannelAdapter.java
@@ -1,13 +1,13 @@
 package org.springframework.integration.smpp;
 
 import org.jsmpp.bean.DeliverSm;
-import org.jsmpp.extra.ProcessRequestException;
-import org.jsmpp.session.MessageReceiverListener;
+import org.jsmpp.bean.DeliveryReceipt;
+import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.integration.endpoint.AbstractEndpoint;
-import org.springframework.integration.smpp.session.AbstractMessageReceiverListener;
 import org.springframework.integration.smpp.session.ExtendedSmppSession;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.util.Assert;
 
 /**
@@ -37,16 +37,6 @@ public class SmppInboundChannelAdapter extends AbstractEndpoint {
 		this.messagingTemplate = new MessagingTemplate(this.channel);
 	}
 
-	/**
-	 * the dispatcher for inbound messages to Spring Integration clients
-	 */
-	private MessageReceiverListener messageReceiverListener = new AbstractMessageReceiverListener() {
-		@Override
-		public void onAcceptDeliverSm(DeliverSm deliverSm) throws ProcessRequestException {
-
-		}
-	};
-
 	@Override
 	protected void onInit() throws Exception {
 		Assert.notNull(this.smppSession, "the 'smppSession' property must be set");
@@ -57,13 +47,29 @@ public class SmppInboundChannelAdapter extends AbstractEndpoint {
 		this.smppSession = s;
 	}
 
+	private AbstractReceivingMessageListener abstractReceivingMessageListener =
+			new AbstractReceivingMessageListener() {
+				@Override
+				protected void onDeliveryReceipt(DeliverSm deliverSm, String ogMessageId, DeliveryReceipt deliveryReceipt) throws Exception {
+					// noop don't care
+				}
+
+				@Override
+				protected void onTextMessage(DeliverSm deliverSm, String txtMessage) throws Exception {
+					// todo include other contents!
+					Message<?> msg = MessageBuilder.withPayload(txtMessage).setHeader("deliverSm", deliverSm).build();
+					messagingTemplate.send(msg);
+				}
+			};
+
 	@Override
 	protected void doStart() {
-		this.smppSession.addMessageReceiverListener(messageReceiverListener);
+		this.smppSession.addMessageReceiverListener(this.abstractReceivingMessageListener);
+		this.smppSession.start();
 	}
 
 	@Override
 	protected void doStop() {
-
+		this.smppSession.stop();
 	}
 }
---
+++ spring-integration-smpp/src/test/java/org/springframework/integration/smpp/TestSmppInboundChannelAdapter.java
@@ -0,0 +1,56 @@
+package org.springframework.integration.smpp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.MessagingException;
+import org.springframework.integration.config.ConsumerEndpointFactoryBean;
+import org.springframework.integration.core.MessageHandler;
+import org.springframework.integration.core.SubscribableChannel;
+import org.springframework.integration.endpoint.AbstractEndpoint;
+import org.springframework.integration.handler.MessageHandlerChain;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration("classpath:TestSmppInboundChannelAdapter-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestSmppInboundChannelAdapter {
+
+	@Autowired @Qualifier("inbound")
+	SubscribableChannel messageChannel;
+
+	@Autowired
+	ApplicationContext context ;
+
+	@Test
+	public void testReceiving () throws Throwable {
+		messageChannel.subscribe(new MessageHandler(){
+			public void handleMessage(Message<?> message) throws MessagingException {
+			 System.out.println(message.getPayload().toString())  ;
+			}
+		});
+		Thread.sleep(1000 * 10);
+
+	/*	ConsumerEndpointFactoryBean consumerEndpointFactoryBean =
+				new ConsumerEndpointFactoryBean();
+		consumerEndpointFactoryBean.setInputChannel(this.messageChannel);
+		consumerEndpointFactoryBean.setBeanFactory( context);
+		consumerEndpointFactoryBean.setHandler( new MessageHandler(){
+			public void handleMessage(Message<?> message) throws MessagingException {
+			 System.out.println(message.getPayload().toString())  ;
+			}
+		});
+
+		AbstractEndpoint abstractEndpoint =
+				consumerEndpointFactoryBean.getObject();
+
+		this.messageChannel.
+
+		abstractEndpoint.start();*/
+
+	}
+}
